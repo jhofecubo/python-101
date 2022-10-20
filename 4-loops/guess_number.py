8f1af9d5a6750504cf77245bca33352a8b8626d6
jhofecubo <102445077+jhofecubo@users.noreply.github.com>
2022-10-19 21:32:21 -0500

    This might be another way to do it
    The output is the same, but I don't know if perhaps I fall into bad practices.

--- 4-loops/guess_number.py
+++ 4-loops/guess_number.py
@@ -1,14 +1,12 @@
-# Guess Number 🔢
-# Codédex
 
+tries=0
 guess = 0
-tries = 0
 
-while guess != 6 and tries < 5:
+while guess != 6 and tries<= 4:
   guess = int(input("Guess the number:  "))
-  tries = tries + 1
+  tries= tries +1
 
-if guess != 6:
-  print("You ran out of tries.")
+if guess == 6:
+    print("You got it!")
 else:
-  print("You got it!")
+    print("No more attemps allowed")
